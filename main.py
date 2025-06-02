91dfea51293ac7d8860a77bf756aa04faed16bc8
ManoloEsS <manolo.estrada.s@gmail.com>
2025-06-01 20:12:07 -0600

    created player file, player triangle on screen

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 import pygame
 
 import constants
+import player
 
 
 def main():
@@ -12,14 +13,25 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    fps = pygame.time.Clock()
+    dt = 0
+
+    # starts in middle of screen
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    player1 = player.Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         screen.fill((0, 0, 0))
+        player1.draw(screen)
 
         pygame.display.flip()
+        fps.tick(60)
+        dt = fps.tick(60) / 1000
 
 
 if __name__ == "__main__":
